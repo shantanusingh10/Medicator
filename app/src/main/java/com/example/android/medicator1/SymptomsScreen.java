10978311f9f84c8c7eefe8eacd348ace2675f4be
Shantanu Singh <shantanusingh994@gmail.com>
2015-12-11 18:43:51 +0530

    add symptoms button functionality with intent

--- app/src/main/java/com/example/android/medicator1/SymptomsScreen.java
+++ app/src/main/java/com/example/android/medicator1/SymptomsScreen.java
@@ -1,31 +1,24 @@
 package com.example.android.medicator1;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SymptomsScreen extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_symptoms_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_symptoms_screen, menu);
         return true;
-    }
-    public void symptomsScreen(View view){
-        Intent sympScreen = new Intent(this,SymptomsScreen.class);
-        startActivity(sympScreen);
     }
 
     @Override
